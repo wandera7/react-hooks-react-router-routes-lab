2ffc556cbd0ac5bf389fca8b43fc755d5fdad327
wandera7 <julius.wandera@student.moringaschool.com>
2022-07-19 17:22:30 +0300

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <div>
+     <h1>Movies Page</h1>
+     {movies.map((movie,index)=>{
+      return (
+        <div key={index}>
+        <h2>{movie.title} Time:{movie.time}min</h2>
+        <ul>
+          {movie.genres.map((genre,index)=><li key={index}>{genre}</li>)}
+        </ul>               
+        </div>
+      )
+     })}
+    </div>
+  )
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <nav>
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/movies">Movies</NavLink>
+        <NavLink exact to="/directors">Directors</NavLink>
+        <NavBar exact to="/actors">Actors</NavBar>
+    </nav>
+  )
 }
 
 export default NavBar;
